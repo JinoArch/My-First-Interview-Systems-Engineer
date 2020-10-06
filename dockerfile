dd4b32274de2773614168ff8d8667e65e74629cb
jino <Jino.Jose@karglobal.com>
2020-10-06 20:41:23 +0530

    last_phase_skull

---
+++ dockerfile
@@ -0,0 +1,19 @@
+FROM ubuntu:20.04
+
+RUN apt-get update && apt-get install -y openssh-server
+RUN service ssh start
+RUN update-rc.d ssh defaults
+RUN echo 'root:lol' | chpasswd
+RUN adduser jino
+RUN echo 'jino:12345' | chpasswd
+RUN sed -i 's/#*PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
+
+# SSH login fix. Otherwise user is kicked off after login
+RUN sed -i 's@session\s*required\s*pam_loginuid.so@session optional pam_loginuid.so@g' /etc/pam.d/sshd
+
+ENV NOTVISIBLE="in users profile"
+RUN echo "export VISIBLE=now" >> /etc/profile
+
+EXPOSE 22
+CMD ["/usr/sbin/sshd", "-D"]
+
--- qblock_api.py
+++ qblock_api.py
@@ -1,10 +1,9 @@
-#This scripts belongs to Jino and no one else is responsible if something goes wrong
-
 import flask
 import docker
 import os
 from flask import request, session
 import random
+import time
 
 link = flask.Flask(__name__)
 link.config["DEBUG"] = True
@@ -46,14 +45,20 @@ def jin():
                 if "mem" in session:
                     mem=session["mem"]
                     client = docker.from_env()
-                    p=client.containers.run("ubuntu:latest", stdin_open = True, tty = True, detach=True, entrypoint='/bin/bash', cpu_count=int(cpu), mem_limit=mem, privileged=True, ports={'22/tcp': ('127.0.0.1', 1331)})
+                    p=client.containers.run("ssh_ubuntu", stdin_open = True, tty = True, detach=True, entrypoint='/bin/bash', cpu_count=int(cpu), mem_limit=mem, privileged=True, ports={'22/tcp': ('127.0.0.1', 9876)})
                     j=str(p).split( )
                     q=j[1][:-1]
                     print (q)
                     container = client.containers.get(str(q))
                     e=vars( container )["attrs"]["NetworkSettings"]["Networks"]["bridge"]["IPAddress"]
                     print ("IP address assigned:"+e)
-                    return "Docker container provisioned"
+                    os.system('sudo docker exec -it '+str(q)+' /bin/bash -c "service ssh start; apt-get install sshpass"')
+                    if 'pass' not in request.args:
+                        return ("no pass provided to login via ssh")
+                    else:
+                        os.system('sshpass -p '+str(request.args.get('pass'))+' ssh -o StrictHostKeyChecking=no '+request.args.get('USER')+'@'+e+' -C "exit"')
+                        return ("successfully logged in using ssh with pass:"+request.args.get('pass')+", assigned IP "+str(e)+" port 22"+", Container ID: "+str(q))
+                    return "Docker Container provisioned and tested SSH access"
                 else:
                     return "mem session went wrong"
             else:
